0458c86dcb1e568464b99c87d18b35238cee5731
samkischuk <samkischuk12@gmail.com>
2026-06-30 10:01:47 -0700

    Update paper.js

--- js/paper.js
+++ js/paper.js
@@ -3,5 +3,10 @@ const paperItems = [
         src: "/images/paper/BravermanIllustration.jpg",
         title: "Braverman Illustration",
         category: "advertisements"
+    },
+    {
+        src: "/images/paper/MtBakerPost.jpg",
+        title: "Mt. Baker Postcard",
+        category: "postcards"
     }
 ];
