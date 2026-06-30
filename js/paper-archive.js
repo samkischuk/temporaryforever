5b120ddb8e8136e90750f50a23da701955e66492
samkischuk <samkischuk12@gmail.com>
2026-06-30 16:52:19 -0700

    Add new matchbook items to paper archive

--- js/paper-archive.js
+++ js/paper-archive.js
@@ -4,16 +4,32 @@ const paperItems = [
         title: "Braverman Illustration",
         category: "advertisements"
     },
+
     {
         src: "/images/paper/mtbakerfront.jpg",
         hoverSrc: "/images/paper/mtbakerback.jpg",
         title: "Mt. Baker Postcard",
         category: "postcards"
     },
+
     {
         src: "/images/paper/majestichotelfront.jpg",
         hoverSrc: "/images/paper/majestichotelback.jpg",
         title: "Majestic Hotel Postcard",
         category: "postcards"
+    },
+
+    {
+        src: "/images/paper/cvinnfront.jpg",
+        hoverSrc: "/images/paper/cvinnback.jpg",
+        title: "CV Inn Matchbook",
+        category: "matchbooks"
+    },
+
+    {
+        src: "/images/paper/ginosfront.jpg",
+        hoverSrc: "/images/paper/ginosback.jpg",
+        title: "Gino's Matchbook",
+        category: "matchbooks"
     }
 ];
