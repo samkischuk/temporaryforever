18587e2a34a795ac41c2bb697614c71200e24714
samkischuk <samkischuk12@gmail.com>
2026-06-29 21:53:15 -0700

    Rearrange photo entries in photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,34 +1,43 @@
 const photos = [
+
     {
-        file: "EasterChildren.jpg",
-        title: "Easter Children"
+        file: "PipeMan2.jpg",
+        title: "Pipe Man II"
     },
-    {
-        file: "EasterChildren2.jpg",
-        title: "Easter Children II"
-    },
-    {
-        file: "BaseballBoy.jpg",
-        title: "Baseball Boy"
-    },
-    {
-        file: "BaseballKids.jpg",
-        title: "Baseball Kids"
-    },
-    {
-        file: "HolidayDinner.jpg",
-        title: "Holiday Dinner"
-    },
-    {
-        file: "YachtClubFamily.jpg",
-        title: "Yacht Club Family"
-    },
+
     {
         file: "PipeMan.jpg",
         title: "Pipe Man"
     },
+
     {
-        file: "PipeMan2.jpg",
-        title: "Pipe Man II"
+        file: "YachtClubFamily.jpg",
+        title: "Yacht Club Family"
+    },
+
+    {
+        file: "HolidayDinner.jpg",
+        title: "Holiday Dinner"
+    },
+
+    {
+        file: "BaseballKids.jpg",
+        title: "Baseball Kids"
+    },
+
+    {
+        file: "BaseballBoy.jpg",
+        title: "Baseball Boy"
+    },
+
+    {
+        file: "EasterChildren2.jpg",
+        title: "Easter Children II"
+    },
+
+    {
+        file: "EasterChildren.jpg",
+        title: "Easter Children"
     }
+
 ];
