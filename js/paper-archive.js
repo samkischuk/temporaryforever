b4d70b7f3b3ccff21753d3fd327e768c26ab88a7
samkischuk <samkischuk12@gmail.com>
2026-06-30 09:28:55 -0700

    Refactor paper archive image handling and lightbox

--- js/paper-archive.js
+++ js/paper-archive.js
@@ -1,17 +1,58 @@
-let currentImageIndex = 0;
+let currentPaperIndex = 0;
 
-const gallery = document.getElementById("paper-gallery");
+const paperGallery = document.getElementById("paper-gallery");
 
-paperItems.forEach((item, index) => {
+let displayedPaperItems = paperItems;
+
+if (window.location.pathname.includes("/advertisements/")) {
+    displayedPaperItems = paperItems.filter(item => item.category === "advertisements");
+}
+
+if (window.location.pathname.includes("/postcards/")) {
+    displayedPaperItems = paperItems.filter(item => item.category === "postcards");
+}
+
+displayedPaperItems.forEach((item, index) => {
     const img = document.createElement("img");
 
     img.src = item.src;
     img.alt = item.title;
     img.className = "thumbnail paper-thumbnail";
 
-    img.onclick = function () {
+    img.addEventListener("click", function () {
         openLightbox(index);
-    };
+    });
 
-    gallery.appendChild(img);
+    paperGallery.appendChild(img);
 });
+
+function openLightbox(index) {
+    currentPaperIndex = index;
+
+    document.getElementById("lightbox-img").src = displayedPaperItems[index].src;
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function previousImage(event) {
+    event.stopPropagation();
+
+    currentPaperIndex =
+        (currentPaperIndex - 1 + displayedPaperItems.length) % displayedPaperItems.length;
+
+    document.getElementById("lightbox-img").src =
+        displayedPaperItems[currentPaperIndex].src;
+}
+
+function nextImage(event) {
+    event.stopPropagation();
+
+    currentPaperIndex =
+        (currentPaperIndex + 1) % displayedPaperItems.length;
+
+    document.getElementById("lightbox-img").src =
+        displayedPaperItems[currentPaperIndex].src;
+}
