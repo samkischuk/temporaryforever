98cf5ebf9bba1d76080ab8486febb101d704e287
samkischuk <samkischuk12@gmail.com>
2026-06-29 21:34:18 -0700

    Create photos array with image details
    Added an array of photo objects with file names and titles.

---
+++ js/photos.js
@@ -0,0 +1,18 @@
+const photos = [
+    {
+        file: "EasterChildren.jpg",
+        title: "Easter Children"
+    },
+    {
+        file: "EasterChildren2.jpg",
+        title: "Easter Children II"
+    },
+    {
+        file: "BaseballBoy.jpg",
+        title: "Baseball Boy"
+    },
+    {
+        file: "BaseballKids.jpg",
+        title: "Baseball Kids"
+    }
+];
