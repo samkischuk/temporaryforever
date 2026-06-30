8bdf85829de51234bd660b4408df38b23904df29
samkischuk <samkischuk12@gmail.com>
2026-06-29 21:55:52 -0700

    Improve slideshow image handling in slideshow.js
    Refactor slideshow image update logic for clarity.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,10 +1,25 @@
+// Homepage Slideshow
+
 let slideIndex = 0;
 
-function showNextSlide() {
-    slideIndex = (slideIndex + 1) % photos.length;
+const slideshowImage = document.getElementById("slideshow-image");
 
-    document.getElementById("slideshow-image").src =
-        "images/" + photos[slideIndex].file;
+// Show the newest photo immediately
+slideshowImage.src = "images/" + photos[0].file;
+slideshowImage.alt = photos[0].title;
+
+// Change photos every 2 seconds
+function showNextSlide() {
+
+    slideIndex++;
+
+    if (slideIndex >= photos.length) {
+        slideIndex = 0;
+    }
+
+    slideshowImage.src = "images/" + photos[slideIndex].file;
+    slideshowImage.alt = photos[slideIndex].title;
+
 }
 
 setInterval(showNextSlide, 2000);
