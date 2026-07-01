1751ec0e310683c57353a0cb5c63ed58fdbaf2e8
samkischuk <samkischuk12@gmail.com>
2026-07-01 16:52:01 -0700

    Remove titles from photo objects
    Removed titles from multiple photo objects in the array.

--- js/photos.js
+++ js/photos.js
@@ -2,343 +2,294 @@ const photos = [
 
     {
         file: "CuteCoupleWithHat.jpg",
-        title: "Cute Couple with Hat",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "TwoMenSmoke.jpg",
-        title: "Two Men Smoke",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "ManFishes.jpg",
-        title: "Man Fishes",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "RidingOutsideKid.jpg",
-        title: "Riding Outside Kid",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BasketCarryKids.jpg",
-        title: "Basket Carry Kids",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BoysBestFriend.jpg",
-        title: "Boy's Best Friend",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "PuppyInYard.jpg",
-        title: "Puppy in Yard",
         medium: "photograph",
         category: "animals"
     },
 
     {
         file: "CarnivalPlane.jpg",
-        title: "Carnival Plane",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BabyOveralls.jpg",
-        title: "Baby Overalls",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "InThePews.jpg",
-        title: "In the Pews",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "LinedUpChurch.jpg",
-        title: "Lined Up Church",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BoysAndGirls.jpg",
-        title: "Boys and Girls",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "PrayerHands.jpg",
-        title: "Prayer Hands",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "FamilyFlick.jpg",
-        title: "Family Flick",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "HoldingBaby2.jpg",
-        title: "Holding Baby II",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "HoldingBaby.jpg",
-        title: "Holding Baby",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Bassinet.jpg",
-        title: "Bassinet",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "AllSmilesBaby.jpg",
-        title: "All Smiles Baby",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "RingForBaby.jpg",
-        title: "Ring for Baby",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Curlers.jpg",
-        title: "Curlers",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Wavy.jpg",
-        title: "Wavy",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "OnTheRocks.jpg",
-        title: "On the Rocks",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Garage.jpg",
-        title: "Garage",
         medium: "photograph",
         category: "places"
     },
 
     {
         file: "SmilingLadies.jpg",
-        title: "Smiling Ladies",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "FamilyTies.jpg",
-        title: "Family Ties",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Tiger.jpg",
-        title: "Tiger",
         medium: "photograph",
         category: "animals"
     },
 
     {
         file: "PolarBear.jpg",
-        title: "Polar Bear",
         medium: "photograph",
         category: "animals"
     },
 
     {
         file: "RaccoonHat.jpg",
-        title: "Raccoon Hat",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "AxeMan.jpg",
-        title: "Axe Man",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "LivingRoom.jpg",
-        title: "Living Room",
         medium: "photograph",
         category: "interiors"
     },
 
     {
         file: "Clown2.jpg",
-        title: "Clown II",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "Clown.jpg",
-        title: "Clown",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "StainedGlass.jpg",
-        title: "Stained Glass",
         medium: "photograph",
         category: "places"
     },
 
     {
         file: "HornSection.jpg",
-        title: "Horn Section",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "InTheBand.jpg",
-        title: "In the Band",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "DeskSpace.jpg",
-        title: "Desk Space",
         medium: "photograph",
         category: "interiors"
     },
 
     {
         file: "OfficeSpace.jpg",
-        title: "Office Space",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "GirlInDress.jpg",
-        title: "Girl in Dress",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "GlowInTheDark.jpg",
-        title: "Glow in the Dark",
         medium: "photograph",
         category: "objects"
     },
 
     {
         file: "RockFace.jpg",
-        title: "Rock Face",
         medium: "photograph",
         category: "places"
     },
 
     {
         file: "ManWithCamera.jpg",
-        title: "Man with Camera",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "PipeMan2.jpg",
-        title: "Pipe Man II",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "PipeMan.jpg",
-        title: "Pipe Man",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "YachtClubFamily.jpg",
-        title: "Yacht Club Family",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "HolidayDinner.jpg",
-        title: "Holiday Dinner",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BaseballKids.jpg",
-        title: "Baseball Kids",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "BaseballBoy.jpg",
-        title: "Baseball Boy",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "EasterChildren2.jpg",
-        title: "Easter Children II",
         medium: "photograph",
         category: "people"
     },
 
     {
         file: "EasterChildren.jpg",
-        title: "Easter Children",
         medium: "photograph",
         category: "people"
     }
