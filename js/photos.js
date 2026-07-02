2782e5033d3ee3ffe9cf1ed38e9c9f4a4cdafa7a
samkischuk <samkischuk12@gmail.com>
2026-07-01 19:32:33 -0700

    Add multiple photograph entries to photos.js

--- js/photos.js
+++ js/photos.js
@@ -292,6 +292,150 @@ const photos = [
         file: "EasterChildren.jpg",
         medium: "photograph",
         category: "people"
+    },
+
+    {
+        file: "ThreeBrothersOnCar.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "CoupleOnTheShore.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "TwoGuysOnBeach.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "TwoGirlsOnTheBeach.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "BarnPic.jpg",
+        medium: "photograph",
+        category: "places"
+    },
+
+    {
+        file: "WomanAndKidInYard.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "BoyAndCat.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "BackyardHang.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "TransportationFade.jpg",
+        medium: "photograph",
+        category: "objects"
+    },
+
+    {
+        file: "ThreeKidsThreePuppies.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "PuppyLove.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "FourGirlsAtHouse.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "GardenParty.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "FourSiblingsInGarden.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "Waterfalls.jpg",
+        medium: "photograph",
+        category: "places"
+    },
+
+    {
+        file: "SiblingsOnTheFarm.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "FunnyGirlSmile.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "SiblingGroupPic.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "FiveSistersInField.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "ThreeLadiesAtBarn.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "ViewFromRoadside.jpg",
+        medium: "photograph",
+        category: "places"
+    },
+
+    {
+        file: "SnowySiblings.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "SnowyDrive.jpg",
+        medium: "photograph",
+        category: "people"
+    },
+
+    {
+        file: "SnowDayBoy.jpg",
+        medium: "photograph",
+        category: "people"
     }
 
 ];
